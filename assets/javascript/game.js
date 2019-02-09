9ed04f99d168ca8e48b52b69a3f3bf9f69f18e8a
KMZ <mzlatnik@gmail.com>
2019-02-09 12:34:28 -0800

    progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,99 +7,119 @@ var activeWord = "";
 var guessList = [];
 var complete = 0;
 
-var shadow = " _ ";
+var shadow = [];
+var shadowstring;
+var wordLength = 0;
 var letter = "";
-
 
 //create an object that contains the basic functions of the game
 var game = {
 
-    word: ['aardvark', 'baby', 'cactus'],
-    usedWord: [], 
-    index: 0,
-    replace: 0,
-    
-    madLib: {
-        4: " _ _ _ _",
-        5: " _ _ _ _ _",
-        6: " _ _ _ _ _ _",
-        7: " _ _ _ _ _ _ _",
-        8: " _ _ _ _ _ _ _ _",
-        9: " _ _ _ _ _ _ _ _ _",
-        10:" _ _ _ _ _ _ _ _ _ _",
-        11:" _ _ _ _ _ _ _ _ _ _ _",
-        12:" _ _ _ _ _ _ _ _ _ _ _ _"
-    },
-    
+    //array of native Oregon wildlife
+    word: ['antelope', 'beaver', 'bobcat', 'cougar', 'deer', 'eagle', 'elk', 'frog', 'hawk', 'lynx', 'moose', 'newt', 'osprey', 'pelican', 'ringtail', 'salamander', 'seal', 'turtle', 'weasel'],
+    usedWord: [],
 
-    // selects a random word from the list, stores as activeWord, and moves it from word to usedWord list:
-    wordSelect: function() {         
-        if (this.word.length > 0) {
-            activeWord = this.word[Math.floor(Math.random() * this.word.length)];
-            console.log(activeWord);
-            var item = this.word.indexOf(activeWord);
-            this.word.splice(item,1);
-            this.usedWord.push(activeWord);
-            console.log(this.word);
-            console.log(this.usedWord)
-        }
-        /* else {
-            <break>
-        } */
-    },        
-    letterTest: function() {
-        var chars = activeWord.length; complete = activeWord.length;
-        console.log(chars)
-        shadow =game.madLib[chars];
-        document.getElementById("textarea").value = shadow;
+    letsPlay: function () {
 
-        if (activeWord.includes(letter)) {
-
-            for (var i = 0; i < chars;  i++) {
-                game.index = activeWord.indexOf(letter,i);
-                game.replace = ((game.index + 1) * 2);
-                game.fillBlank();                
-            }
-
-        }
-
-        else if (guessList.includes(letter)) {
-            alert("You already guessed this letter.");        
-        }
-
-        else {
-            guessList.push(letter);
-  
-        }
-        },
+        function setUp() {
+            if (activeWord === "") {    
+                function newWord() {
+                    // selects a random word from the list, stores as activeWord, and moves it from word to usedWord list
+                    if (game.word.length > 0) {
+                        activeWord = game.word[Math.floor(Math.random() * game.word.length)];
+                        console.log(activeWord);
+                        wordLength = activeWord.length
+                        var item = game.word.indexOf(activeWord);
+                        game.word.splice(item, 1);
+                        game.usedWord.push(activeWord);
+                    }
+                    else {
+                        alert("You've completed the game!");
+                    };
+                };
+                
+                function shadowBox() {
+                    for (var i = 0; i < wordLength; i++) {
+                        shadow[i] = "_";
+                    }
         
-        fillBlank: function () {
-            shadow.splice(game.replace,1,letter);
-            complete--;
-        }
-    }        
+                    // putting in a string
+                    shadowstring = shadow.join(" ");
+                    document.getElementById("wordBox").innerHTML = shadowstring;
+                };
+                shadowBox()
+                newWord()
+                shadowBox()
+            };
+        };
+        setUp()
+        function lettertest() {
+            if (guessList.includes(letter)) {
+                alert("You already guessed this letter.");
+            } else {
+                guessList.push(letter);
+                guessCount--;
+                document.getElementById("guessCount").innerHTML = "Remaining guesses: " + guessCount;
+                var guessstring = guessList.join(" ");
+                document.getElementById("guesses").innerHTML = "Letters guessed: " + guessstring;
+            
+                for (var i = 0; i < activeWord.length; i++) {
+                    if (activeWord[i] === letter) {
+                        shadow[i] = letter;
+                        complete++;
+                        console.log(complete)
+                        console.log(shadow);                      
+                    }
+                }
+                shadowstring = shadow.join(" ");
+                console.log(shadowstring)
+                document.getElementById("wordBox").innerHTML = shadowstring;
+            }; 
+        };
+        lettertest()
 
-    //sub-function - creates a string to represent the word
-//function 2 - validates the guess
-//function 3 - compares 
+        function reset() {
+            activeWord = "";
+            guessCount = 12;
+            guessList = [];
+            complete = 0;
+            var guessstring = (" ")
+            document.getElementById("guesses").innerHTML = "Letters guessed: " + guessstring;
+            var shadowstring = " "
+            document.getElementById("wordBox").innerHTML = shadowstring;
+        };          
+        
+        // reset settings  and add win after completion of each word:
+        if (complete === activeWord.length) {
+            alert(activeWord + "!")
+            wins++
+            document.getElementById("wins").innerHTML = "Wins: " + wins;
+            reset()
+            setUp()
+        };
+
+        // reset settings if out of guesses
+        if (guessCount < 1) {
+            alert("You ran out of guesses!")
+            reset()
+            setUp()
+        };
+    }
+};
 
 
 
+// MAIN PROCESS
+// ==============================================================================
 
+// Captures keyboard input.
+document.onkeyup = function (event) {
 
- // MAIN PROCESS
-      // ==============================================================================
+    // Captures the key press, converts it to lowercase, and saves it to a variable.
+    letter = event.key.toLowerCase();
 
-      // Captures keyboard input.
-      document.onkeyup = function(event) {
+    game.letsPlay()
 
-        // Captures the key press, converts it to lowercase, and saves it to a variable.
-        letter = event.key.toLowerCase();
-
-        game.wordSelect()
-
-        game.letterTest()
-    
-    };
+};
 
 
